4193f15de47ac9b94871a0247c5aadbfbde38513
AlexVentisca <72811107+AlexVentisca@users.noreply.github.com>
2023-02-02 14:19:54 +0100

    Prueba
    Prueba

---
+++ prueba.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  2 14:16:42 2023
+
+@author: alejandro
+"""
+
+#hola esta es una prueba realizada en python
+print("hola esta es una prueba realizada en python")
